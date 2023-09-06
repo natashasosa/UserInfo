7e16e90f8676d771124d508ede6978cfb787cae5
natashasosa <natasha.sosa@poatek.com>
2023-09-06 14:52:07 -0300

    first commit

---
+++ UserInfo/UserInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserInfoApp.swift
+//  UserInfo
+//
+//  Created by Natasha Rebelo on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct UserInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
