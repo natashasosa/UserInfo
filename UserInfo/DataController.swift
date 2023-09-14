c8264111505e75fd767f6d173d90a1a77061ed98
natashasosa <natasha.sosa@poatek.com>
2023-09-14 15:57:01 -0300

    Make Data Model with entities, Data Controller and initializer preparing to migrate the data structure to Core Data

---
+++ UserInfo/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  UserInfo
+//
+//  Created by Natasha Rebelo on 14/09/23.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "UserInfo")
+
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
